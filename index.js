1bcf81a6ac184b84ad5b4e371b7145559114f0d6
Shirish Pandey <shirishpandey@gmail.com>
2016-01-15 20:54:30 +0545

    added nDecimals and nReduce APIs, updated the readme file accordingly

--- index.js
+++ index.js
@@ -1,93 +1,202 @@
-(function() {
+var geojson = require('geojson-validation');
+var simplify = require('simplify-geometry');
 
-  function parse(t, precision) {
+(function () {
 
-    function point(p) {
-      return p.map(function(e) {
-        return 1 * parseFloat(e).toFixed(precision);
-      });
+    function isGeoJSON(data) {
+        var validGeoJSON = 0;
+        if (!geojson.valid(data)) {
+            validGeoJSON = 0;
+        } else {
+            if (!/^[0-9,\.\+\- \[\]\"\']+$/.test(JSON.stringify(data.coordinates))) {
+                validGeoJSON = 0;
+            } else {
+                validGeoJSON = 1;
+            }
+        }
+        return validGeoJSON;
     }
 
-    function multi(l) {
-      return l.map(point);
+    function nDecimals(t, precision) {
+
+        if (isGeoJSON(t)) {
+
+            function point(p) {
+                return p.map(function (e) {
+                    return 1 * parseFloat(e).toFixed(precision);
+                });
+            }
+
+            function multi(l) {
+                return l.map(point);
+            }
+
+            function poly(p) {
+                return p.map(multi);
+            }
+
+            function multiPoly(m) {
+                return m.map(poly);
+            }
+
+            function geometry(obj) {
+                if (!obj) {
+                    return {};
+                }
+
+                switch (obj.type) {
+                    case "Point":
+                        obj.coordinates = point(obj.coordinates);
+                        return obj;
+                    case "LineString":
+                    case "MultiPoint":
+                        obj.coordinates = multi(obj.coordinates);
+                        return obj;
+                    case "Polygon":
+                    case "MultiLineString":
+                        obj.coordinates = poly(obj.coordinates);
+                        return obj;
+                    case "MultiPolygon":
+                        obj.coordinates = multiPoly(obj.coordinates);
+                        return obj;
+                    case "GeometryCollection":
+                        obj.geometries = obj.geometries.map(geometry);
+                        return obj;
+                    default:
+                        return obj;
+                }
+            }
+
+            function feature(obj) {
+                obj.geometry = geometry(obj.geometry);
+                return obj
+            }
+
+            function featureCollection(f) {
+                f.features = f.features.map(feature);
+                return f;
+            }
+
+            function geometryCollection(g) {
+                g.geometries = g.geometries.map(geometry);
+                return g;
+            }
+
+            if (!t) {
+                return t;
+            }
+
+            switch (t.type) {
+                case "Feature":
+                    return feature(t);
+                case "GeometryCollection":
+                    return geometryCollection(t);
+                case "FeatureCollection":
+                    return featureCollection(t);
+                case "Point":
+                case "LineString":
+                case "Polygon":
+                case "MultiPoint":
+                case "MultiPolygon":
+                case "MultiLineString":
+                    return geometry(t);
+                default:
+                    return t;
+            }
+
+        } else {
+            return t;
+        }
+
+
     }
 
-    function poly(p) {
-      return p.map(multi);
+    function nReduce(t, tolerance) {
+
+        if (isGeoJSON(t)) {
+
+            function multi(l) {
+                return simplify(l, tolerance);
+            }
+
+            function poly(p) {
+                return p.map(multi);
+            }
+
+            function multiPoly(m) {
+                return m.map(poly);
+            }
+
+            function geometry(obj) {
+                if (!obj) {
+                    return {};
+                }
+
+                switch (obj.type) {
+                    case "LineString":
+                    case "MultiPoint":
+                        obj.coordinates = multi(obj.coordinates);
+                        return obj;
+                    case "Polygon":
+                    case "MultiLineString":
+                        obj.coordinates = poly(obj.coordinates);
+                        return obj;
+                    case "MultiPolygon":
+                        obj.coordinates = multiPoly(obj.coordinates);
+                        return obj;
+                    case "GeometryCollection":
+                        obj.geometries = obj.geometries.map(geometry);
+                        return obj;
+                    default:
+                        return obj;
+                }
+            }
+
+            function feature(obj) {
+                obj.geometry = geometry(obj.geometry);
+                return obj
+            }
+
+            function featureCollection(f) {
+                f.features = f.features.map(feature);
+                return f;
+            }
+
+            function geometryCollection(g) {
+                g.geometries = g.geometries.map(geometry);
+                return g;
+            }
+
+            if (!t) {
+                return t;
+            }
+
+            switch (t.type) {
+                case "Feature":
+                    return feature(t);
+                case "GeometryCollection":
+                    return geometryCollection(t);
+                case "FeatureCollection":
+                    return featureCollection(t);
+                case "Point":
+                case "LineString":
+                case "Polygon":
+                case "MultiPoint":
+                case "MultiPolygon":
+                case "MultiLineString":
+                    return geometry(t);
+                default:
+                    return t;
+            }
+
+        } else {
+            return t;
+        }
+
+
     }
 
-    function multiPoly(m) {
-      return m.map(poly);
-    }
+    module.exports.nDecimals = nDecimals;
+    module.exports.nReduce = nReduce;
 
-    function geometry(obj) {
-      if (!obj) {
-        return {};
-      }
-      
-      switch (obj.type) {
-        case "Point":
-          obj.coordinates = point(obj.coordinates);
-          return obj;
-        case "LineString":
-        case "MultiPoint":
-          obj.coordinates = multi(obj.coordinates);
-          return obj;
-        case "Polygon":
-        case "MultiLineString":
-          obj.coordinates = poly(obj.coordinates);
-          return obj;
-        case "MultiPolygon":
-          obj.coordinates = multiPoly(obj.coordinates);
-          return obj;
-        case "GeometryCollection":
-          obj.geometries = obj.geometries.map(geometry);
-          return obj;
-        default :
-          return {};
-      }
-    }
-
-    function feature(obj) {
-      obj.geometry = geometry(obj.geometry);
-      return obj
-    }
-
-    function featureCollection(f) {
-      f.features = f.features.map(feature);
-      return f;
-    }
-
-    function geometryCollection(g) {
-      g.geometries = g.geometries.map(geometry);
-      return g;
-    }
-
-    if (!t) {
-      return t;
-    }
-
-    switch (t.type) {
-      case "Feature":
-        return feature(t);
-      case "GeometryCollection" :
-        return geometryCollection(t);
-      case "FeatureCollection" :
-        return featureCollection(t);
-      case "Point":
-      case "LineString":
-      case "Polygon":
-      case "MultiPoint":
-      case "MultiPolygon":
-      case "MultiLineString":
-        return geometry(t);
-      default :
-        return t;
-    }
-      
-  }
-
-  module.exports = parse;
-  module.exports.parse = parse;
-
-}());
+} ());
